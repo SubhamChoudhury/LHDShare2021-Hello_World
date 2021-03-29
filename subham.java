d01c24290fd31d0398738fee13a991369021b560
Subham Choudhury <55877612+SubhamChoudhury@users.noreply.github.com>
2021-03-29 20:11:11 +0530

    Hello World In Java

---
+++ subham.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main 
+{
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello, World");
+        String food ="Biryani";
+        String name;
+        System.out.print("What is your name? ");
+        name=sc.nextLine();
+        System.out.println(name+" loves "+food);
+    }
+}
